1d4a9eb0593b5daf16ecdaa8e1aae146508a68db
Cristian Antonio <tauro24_vs@hotmail.com>
2020-12-19 18:49:48 -0500

    test sub

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import {Renderer2, ElementRef } from '@angular/core';
+
+
+
+
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  hora: string;
+}
+
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +20,68 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'prueba3';
+
+  items = [];
+
+  constructor(private rd: Renderer2,private el: ElementRef) {}
+
+ 
+
+ 
+  onClick(hora, item, i){    
+
+    //console.log(item); 
+    //this.rd.setStyle(item, 'background-color', '#000080'); 
+    this.items.push(i);
+    console.log(this.items);
+
+   
+    }
+
+   
+
+    hacerReserva(){
+      alert("reserva "+ this.items);
+      
+      for(var i = 0;i < this.items.length; i++){
+        console.log(this.items[i] + " "+ this.tiles[this.items[i]].hora)
+        this.tiles[this.items[i]].color = 'reservado';
+      }
+      this.items = [];
+      
+    }
+
+    
+    tiles: Tile[] = [
+      {hora: '08:00', cols: 1, rows: 1, color: 'libre'},
+      {hora: '08:30', cols: 1, rows: 1, color: 'libre'},
+      {hora: '09:00', cols: 1, rows: 1, color: 'reservado'},
+      {hora: '09:30', cols: 1, rows: 1, color: 'libre'},
+      {hora: '10:00', cols: 1, rows: 1, color: 'libre'},
+      {hora: '10:30', cols: 1, rows: 1, color: 'libre'},
+      {hora: '11:00', cols: 1, rows: 1, color: 'libre'},
+      {hora: '11:30', cols: 1, rows: 1, color: 'libre'},
+      {hora: '12:00', cols: 1, rows: 1, color: 'libre'},
+      {hora: '12:30', cols: 1, rows: 1, color: 'libre'},
+      {hora: '13:00', cols: 1, rows: 1, color: 'libre'},
+      {hora: '13:30', cols: 1, rows: 1, color: 'libre'},
+      {hora: '14:00', cols: 1, rows: 1, color: 'libre'},
+      {hora: '14:30', cols: 1, rows: 1, color: 'libre'},
+      {hora: '15:00', cols: 1, rows: 1, color: 'libre'},
+      {hora: '15:30', cols: 1, rows: 1, color: 'libre'},
+      {hora: '16:00', cols: 1, rows: 1, color: 'libre'},
+      {hora: '16:30', cols: 1, rows: 1, color: 'libre'},
+      {hora: '17:00', cols: 1, rows: 1, color: 'libre'},
+      {hora: '17:30', cols: 1, rows: 1, color: 'libre'},
+      {hora: '18:00', cols: 1, rows: 1, color: 'libre'},
+      {hora: '19:30', cols: 1, rows: 1, color: 'libre'},
+      {hora: '20:00', cols: 1, rows: 1, color: 'libre'},
+      {hora: '20:30', cols: 1, rows: 1, color: 'libre'},
+      {hora: '21:00', cols: 1, rows: 1, color: 'libre'},
+      {hora: '21:30', cols: 1, rows: 1, color: 'libre'},
+      {hora: '22:00', cols: 1, rows: 1, color: 'libre'},
+    ];
+
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatSliderModule } from '@angular/material/slider';
+import {MatTableModule} from '@angular/material/table';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatButtonModule} from '@angular/material/button';
+
+
+
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatTableModule,
+    MatGridListModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
